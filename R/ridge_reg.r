76a005e0b6d730f9b99146adbda01f6b8501e79b
Grace Yin <graceyin@Graces-MBP-3.wireless.yale.internal>
2018-10-15 21:36:19 -0400

    hw#2

--- R/ridge_reg.r
+++ R/ridge_reg.r
@@ -1,6 +1,6 @@
 #' Fit a Ridge Regression Model
 #'
-#' @description This function passes parameters to the ridge_reg function.
+#' @description This function performs ridge regression function.
 #' @param formula a formula
 #' @param lambda a penalty value 
 #' @param data a data.frame
@@ -18,16 +18,23 @@ ridge_reg = function(formula, lambda, data){
   m <- model.matrix(formula,data)
   y <- matrix(data[,as.character(formula)[2]], ncol=1)
   y<- y[as.numeric(rownames(m)),, drop = FALSE]
-   
+ 
+# Use SVD to calculate the beta   
   svd_obj <- svd(m)
   U <- svd_obj$u
   V <- svd_obj$v
   svals <- svd_obj$d
 
   D <- diag(svals / (svals^2 + lambda))
+  
+# Calculate the beta  
   beta <- V%*% D %*% t(U) %*% y
+
+# Name coefficients
   rownames(beta)<- colnames(m)
   ret <- list(coefficients = beta, lambda = lambda, form = formula)
   class(ret) <- "ridge_reg"
+  
+# Return the result
   ret
 }
